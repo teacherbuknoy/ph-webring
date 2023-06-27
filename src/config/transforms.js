893665b32c1c4e88b788f32fed1b486fecb8b412
Francis Rubio <francis.rubio@dice205.com>
2023-06-27 14:02:07 +0800

    Add feeds opml

--- src/config/transforms.js
+++ src/config/transforms.js
@@ -4,11 +4,24 @@ const path = require('path')
 module.exports = {
   prettier: (content, outputPath) => {
     const extname = path.extname(outputPath)
+    const config = {
+      singleAttributePerLine: false,
+      printWidth: 100,
+      tabWidth: 2,
+      bracketSameLine: true,
+    }
+
+
     switch (extname) {
       case '.html':
       case '.json':
-        const parser = extname.replace(/^./, "")
-        return prettier.format(content, { parser, singleAttributePerLine: false, printWidth: 100 })
+        config.parser = extname.replace(/^./, "")
+        return prettier.format(content, config)
+      case '.xml':
+        config.parser = 'xml'
+        config.plugins = ['@prettier/plugin-xml']
+        config.xmlWhitespaceSensitivity = 'ignore'
+        return prettier.format(content, config)
       default:
         return content
     }
