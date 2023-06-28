6a3f65c4484c93b7039eeea07a83c92a9e35ea65
Francis Rubio <francis.rubio@dice205.com>
2023-06-29 01:12:02 +0800

    Fix base url

--- src/data/meta.js
+++ src/data/meta.js
@@ -1,5 +1,7 @@
-exports.default = {
-  baseUrl: () => process.env.ELEVENTY_ENV === 'production'
-    ? 'https://webring.antaresph.dev'
-    : 'http://localhost:8080'
+const baseUrl = () => process.env.ELEVENTY_ENV === 'production'
+  ? 'https://webring.antaresph.dev'
+  : 'http://localhost:8080'
+
+module.exports = () =>{
+  return { baseUrl: baseUrl() }
 }
