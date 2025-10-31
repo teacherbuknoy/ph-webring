aa10dfc49cae030db50d67d01f6a344eaaffbb38
Francis Rubio <francis.rubio@dice205.com>
2025-10-31 15:53:44 +0800

    fix: update webring removal notice and banner instructions

--- src/data/meta.js
+++ src/data/meta.js
@@ -1,6 +1,6 @@
 const baseUrl = () => process.env.ELEVENTY_ENV === 'production'
   ? 'https://webring.antaresph.dev'
-  : 'http://localhost:8888'
+  : 'http://localhost:8080'
 
 export default () => {
   return { baseUrl: baseUrl() }
